d84667e47d2dea4c3a5b052cf481a81d2bcf2444
Sultan <abylkasievsultan@gmail.com>
2021-03-19 01:09:36 +0600

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import './Footer.css'
+import {NavLink} from 'react-router-dom';
+import facebook from '../../container/img/facebook.png'
+import telegram from '../../container/img/telegram.png'
+import twitter from '../../container/img/twitter.png'
+import youtube from '../../container/img/youtube.png'
+import whatsapp from '../../container/img/whatsapp.png'
+import logo from '../../container/img/logo.png'
+
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                <div className="foot-up">
+                    <div className="container">
+                        <div className="foot">
+                            <div className="foot-block">
+                                <h3 className="footer-title">Меню</h3>
+                                <ul className="foot-nav">
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Главная</NavLink>
+                                    </li>
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Новости</NavLink>
+                                    </li>
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Добавить</NavLink>
+                                    </li>
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Контакты</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="foot-block">
+                                <h3 className="footer-title">Новости</h3>
+                                <ul className="foot-nav">
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Свежие новости</NavLink>
+                                    </li>
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Новости зарубежные</NavLink>
+                                    </li>
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Актуальные новости</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="foot-block">
+                                <h3 className="footer-title">Партнеры</h3>
+                                <ul className="foot-nav">
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Azattyk.kg</NavLink>
+                                    </li>
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Ala-Too24.kg</NavLink>
+                                    </li>
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Sputnik.kg</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="foot-block">
+                                <h3 className="footer-title">О сайте</h3>
+                                <ul className="foot-nav">
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Наши проекты</NavLink>
+                                    </li>
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Наши услуги</NavLink>
+                                    </li>
+                                    <li className="foot-item">
+                                        <NavLink className="foot-link" to="/" exact>Наши достижения</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="foot-down">
+                    <div className="container">
+                        <div className="footer-logo">
+                            <img className="logo" src={logo}/>
+                        </div>
+                        <div className="soc-icons">
+                            <ul className="icons">
+                                <li className="icon">
+                                    <NavLink className="icon-link" to="/" exact>
+                                        <img className="icon-img" src={facebook}/>
+                                    </NavLink>
+                                </li>
+                                <li className="icon">
+                                    <NavLink className="icon-link" to="/" exact>
+                                        <img className="icon-img" src={telegram}/>
+                                    </NavLink>
+                                </li>
+                                <li className="icon">
+                                    <NavLink className="icon-link" to="/" exact>
+                                        <img className="icon-img" src={twitter}/>
+                                    </NavLink>
+                                </li>
+                                <li className="icon">
+                                    <NavLink className="icon-link" to="/" exact>
+                                        <img className="icon-img" src={youtube}/>
+                                    </NavLink>
+                                </li>
+                                <li className="icon">
+                                    <NavLink className="icon-link" to="/" exact>
+                                        <img className="icon-img" src={whatsapp}/>
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import logo from '../../container/img/logo.png'
+import address from '../../container/img/address.png'
+import {NavLink} from 'react-router-dom';
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="wrap">
+                <header className="header">
+                    <div className="header__up">
+                        <div className="container">
+                            <img className="logo" src={logo}/>
+                            <a href="#" className="address top-title"><img src={address}/> Bishkek, Kyrgyzstan</a>
+                            <p className="worktime">пн-пт<br/>10:00-19:00</p>
+                            <a href="#" className="email top-title">welcome@mail.com</a>
+                            <button className="signup">Sign up</button>
+                            <a href="#" className="number">+996 (000) 000000<br/><p className="free-call top-title">Бесплатный звонок</p></a>
+                        </div>
+                    </div>
+                    <div className="header__down">
+                        <div className="container">
+                            <div className="header__nav">
+                                <ul className="nav">
+                                    <li className="nav__item">
+                                        <NavLink className="nav-link" to="/" exact>Главная</NavLink>
+                                    </li>
+                                    <li className="nav__item">
+                                        <NavLink className="nav-link" to="/" exact>Новости</NavLink>
+                                    </li>
+                                    <li className="nav__item">
+                                        <NavLink className="nav-link" to="/" exact>Добавить</NavLink>
+                                    </li>
+                                    <li className="nav__item">
+                                        <NavLink className="nav-link" to="/" exact>Контакты</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="header__bg">
+                        <div className="container">
+                            <div className="header-info">
+                                <h1 className="header-title">Новости</h1>
+                                <div className="hashtags">
+                                    <a href="#" className="tags">#News</a>
+                                    <a href="#" className="tags">#Кыргызстан</a>
+                                    <a href="#" className="tags">#События</a>
+                                    <a href="#" className="tags">#Сми</a>
+                                    <a href="#" className="tags">#Интересное</a>
+                                    <a href="#" className="tags">#Сегодня</a>
+                                    <a href="#" className="tags">#Происшествия</a>
+                                    <a href="#" className="tags">#News</a>
+                                    <a href="#" className="tags">#Кыргызстан</a>
+                                    <a href="#" className="tags">#События</a>
+                                    <a href="#" className="tags">#Сми</a>
+                                    <a href="#" className="tags">#Интересное</a>
+                                    <a href="#" className="tags">#Сегодня</a>
+                                    <a href="#" className="tags">#Происшествия</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Header;
